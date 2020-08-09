6a370ba3f50ffaee3d7cbe807db570c00d689769
YashAPT <yashkumar201301@gmail.com>
2020-08-09 11:25:44 +0530

    initial commit

---
+++ app/src/main/java/com/yash/musiconline/activities/MainActivity.java
@@ -0,0 +1,144 @@
+package com.yash.musiconline.activities;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.yash.musiconline.R;
+import com.yash.musiconline.adapters.NotesAdapter;
+import com.yash.musiconline.database.NotesDatabase;
+import com.yash.musiconline.entities.Note;
+import com.yash.musiconline.listeners.NotesListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements NotesListener {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
+    public static final int REQUEST_CODE_UPDATE_NOTE = 2;
+    public static final int REQUEST_CODE_SHOW_NOTES = 3;
+    private RecyclerView notesRecyclerView;
+    private List<Note> notesList;
+    private NotesAdapter notesAdapter;
+
+    private int noteClickedPosition = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class),
+                        REQUEST_CODE_ADD_NOTE);
+            }
+        });
+
+        notesRecyclerView = findViewById(R.id.notesRecyclerView);
+        notesRecyclerView.setLayoutManager(
+                new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        );
+
+        notesList = new ArrayList<>();
+        notesAdapter = new NotesAdapter(notesList, this);
+        notesRecyclerView.setAdapter(notesAdapter);
+
+        getNotes(REQUEST_CODE_SHOW_NOTES, false);
+
+        EditText inputSearch = findViewById(R.id.inputSearch);
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                notesAdapter.cancelTimer();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (notesList.size() != 0) {
+                    notesAdapter.searchNotes(editable.toString());
+                }
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onNoteClicked(Note note, int position) {
+        noteClickedPosition = position;
+        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+        intent.putExtra("isViewOrUpdate", true);
+        intent.putExtra("note", note);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTE);
+    }
+
+    private void getNotes(final int requestCode, final boolean isNoteDeleted) {
+
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase
+                        .getDatabase(getApplicationContext())
+                        .noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                if (requestCode == REQUEST_CODE_SHOW_NOTES) {
+                    notesList.addAll(notes);
+                    notesAdapter.notifyDataSetChanged();
+                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
+                    notesList.add(0, notes.get(0));
+                    notesAdapter.notifyItemInserted(0);
+                    notesRecyclerView.smoothScrollToPosition(0);
+                } else if (requestCode == REQUEST_CODE_UPDATE_NOTE) {
+                    notesList.remove(noteClickedPosition);
+                    if (isNoteDeleted) {
+                        notesAdapter.notifyItemRemoved(noteClickedPosition);
+                    } else {
+                        notesList.add(noteClickedPosition, notes.get(noteClickedPosition));
+                        notesAdapter.notifyItemChanged(noteClickedPosition);
+                    }
+                }
+            }
+        }
+        new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes(REQUEST_CODE_ADD_NOTE, false);
+        } else if (requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK) {
+            if (data != null) {
+                getNotes(REQUEST_CODE_UPDATE_NOTE, data.getBooleanExtra("isNoteDeleted", false));
+            }
+        }
+    }
+
+}
---
+++ app/src/main/java/com/yash/musiconline/database/NotesDatabase.java
@@ -0,0 +1,25 @@
+package com.yash.musiconline.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.yash.musiconline.dao.NoteDao;
+import com.yash.musiconline.entities.Note;
+
+@Database(entities = Note.class, version = 1, exportSchema = false)
+public abstract class NotesDatabase extends RoomDatabase {
+
+    private static NotesDatabase notesDatabase;
+
+    public static synchronized NotesDatabase getDatabase(Context context) {
+        if (notesDatabase == null) {
+            notesDatabase = Room.databaseBuilder(context, NotesDatabase.class, "notes_db").build();
+        }
+        return notesDatabase;
+    }
+
+    public abstract NoteDao noteDao();
+}
